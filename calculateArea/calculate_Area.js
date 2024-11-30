5cee2b7dcff9c3677b66e7c0ea3d4e824b6b0607
Motoko Miyahisa <mmiyahisa@gmail.com>
2024-11-30 00:50:27 -0500

    grocery calculation

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -4,5 +4,18 @@ function calculateArea(){
     length = parseFloat(document.getElementById('length').value);
     width = parseFloat(document.getElementById('width').value);
     let area = length * width;
-    document.getElementById('result').innerText = 'The area of the rectangle is: ${area}`;
+    
+    document.getElementById('result').innerText = "The total area is "+area;
+}
+let grocery1;
+let grocery2;
+let grocery3;
+function calculateTotalGrocery(){
+    grocery1 = parseFloat(document.getElementById('grocery1').value);
+    grocery2 = parseFloat(document.getElementById('grocery2').value);
+    grocery3 = parseFloat(document.getElementById('grocery3').value);
+    
+    let totalg = grocery1 + grocery2 + grocery3;
+    
+    document.getElementById('resultG').innerText = "The total cost is "+totalg;
 }
